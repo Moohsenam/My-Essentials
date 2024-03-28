b592618fcf87afb6180982b44dfbdb9cd11f20e1
Moohsenam <97410843+Moohsenam@users.noreply.github.com>
2024-03-28 16:53:03 +0330

    Create irsetup.sh

---
+++ irsetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Command 1
+wget "https://raw.githubusercontent.com/radkesvat/ReverseTlsTunnel/master/scripts/install.sh" -O install.sh && chmod +x install.sh && bash install.sh
+
+# Command 2
+nohup ./RTT --iran --lport:443 --sni:splus.ir --password:123 &
+
+# Command 3
+nohup ./RTT --iran --lport:8080 --sni:splus.ir --password:123 &
+
+# Command 4
+nohup ./RTT --iran --lport:8081 --sni:splus.ir --password:123 &
